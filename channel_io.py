e63eb604e6c83ab64bbc98a311534f1388e36305
Andy MacGregor <Andrew_MacGregor@student.uml.edu>
2020-12-10 21:39:34 -0500

    install discord Library + keep one python file

--- channel_io.py
+++ channel_io.py
@@ -1,4 +1,6 @@
-class Channel():
+import discord
+
+class EncouterClient(discord.Client):
     """interract with the channel from the resful API"""
     def __init__(self, channelId):
         #TODO - call GET method to get channel objects
@@ -14,6 +16,18 @@ class Channel():
             pass
         return encounters
 
+    def on_command(self, msg):
+        # TODO call on new message from discord channel,
+        # TODO check for command in cmd_table and dispatch it
+        pass
+
+    def install_command(self, label, fn):
+        """Add a command to the table
+        fn is a function that takes a json string as its only arg"""
+        entry = {label: fn}
+        self.cmd_table.update(entry)
+
 
 if __name__ == "__main__":
-    print("No test yet")
+    print("No Test yet")
+
