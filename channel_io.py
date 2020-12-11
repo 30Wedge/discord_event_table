830fdcdd13cdbfa060bf196e2a481874a204fe61
Andy MacGregor <Andrew_MacGregor@student.uml.edu>
2020-12-10 21:22:11 -0500

    stub out REST and Socket handlers

---
+++ channel_io.py
@@ -0,0 +1,19 @@
+class Channel():
+    """interract with the channel from the resful API"""
+    def __init__(self, channelId):
+        #TODO - call GET method to get channel objects
+        self.chan_obj
+        pass
+
+    def seed_encouter_table(self, num_encounters=100):
+        """Get 100 messages that start with"""
+        encounters = []
+        while len(encounters) < num_encounters:
+            break
+            # TODO Call the Restful API until we have 100 messages starting with !
+            pass
+        return encounters
+
+
+if __name__ == "__main__":
+    print("No test yet")
